14c15c280cbc332dc7cf911bc08dca3916c27d75
david <david.medina1@udea.edu.co>
2020-09-16 15:51:35 -0500

    build methods for all schemas

--- nosqldbengine.py
+++ nosqldbengine.py
@@ -5,7 +5,7 @@ class NosqlDb:
 	and methods for update schemas and store these in db_main.
 	 '''
 	
-	db_main = {'for_1':[],'for_2':[],'for_3':[]}
+	db_main = {'for_s1':[],'for_s2':[],'for_s3':[]}
 
 	def __init__(self):
 		self.s1 = {'key1':[],'key2':[]}
@@ -17,9 +17,29 @@ class NosqlDb:
 		self.s1['key1'].append(value1)
 		self.s1['key2'].append(value2)
 
+	def update_s2(self, value1,value2):
+		self.s2['field1'].append(value1)
+		self.s2['field2'].append(value2)
+
+	def update_s3(self, value1,value2):
+		self.s1['meta1'].append(value1)
+		self.s1['meta2'].append(value2)
+
 	def update_main(self):
+
 		if self.s1['key1'] and self.s1['key2']:
-			NosqlDb.db_main['for_1'].append(self.s1)
+			NosqlDb.db_main['for_s1'].append(self.s1)
+		else:
+			NosqlDb.db_main['for_s1'].append('empty')
+
+		if self.s2['field1'] and self.s2['field2']:
+			NosqlDb.db_main['for_s2'].append(self.s2)
+		else:
+			NosqlDb.db_main['for_s2'].append('empty')
+		if self.s3['meta1'] and self.s3['meta2']:
+			NosqlDb.db_main['for_s3'].append(self.s3)
+		else:
+			NosqlDb.db_main['for_s3'].append('empty')
 
 
 def main():
