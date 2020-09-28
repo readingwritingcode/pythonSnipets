8f44616829b80408d26a05786d6baac03b509daf
david <david.medina1@udea.edu.co>
2020-09-27 20:45:27 -0500

    update to module

--- movefiles.py
+++ movefiles.py
@@ -6,17 +6,23 @@ you simply rename the file by replacing its old directory with its
 target directory.'''
 
 import os
+import sys
 from pathlib import Path 
 
-target_folder = Path("target_folder")
-target_folder.mkdir(parents=True, exist_ok=True) # i have a dude here
-source_folder = Path('.')
+def move_files(target_folder,source_folder, file, file_ext):
+	target_folder = Path(target_folder)
+	target_folder.mkdir(parents=True, exist_ok=True) # i have a dude here
+	source_folder = Path(source_folder)
 
-txt_files = source_folder.glob('*.txt')
-for txt_file in txt_files: 
-	file_name = txt_file.name 
-	target_path = target_folder.joinpath(file_name)
-	print("Moving:"  file_name)
-	print(target_path.exist()) #this raise false
-	txt_file.rename(target_path)
-	print(target_path.exist()) #this raise true, because file has been move rename
+	txt_files = source_folder.glob('*.' + file_ext)
+	for txt_file in txt_files: 
+		file_name = txt_file.name 
+		target_path = target_folder.joinpath(file_name)
+		txt_file.rename(target_path)
+	return os.listdir(target_folder)
+
+def main():
+	move_files(sys.argv[1],sys.argv[2],sys.argv[3])
+
+if __name__ == "__main__":
+	main()
